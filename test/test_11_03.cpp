da4d9705f14afbe7096ad9fc5deecd6f08912c1a
leiyaling <3120275158@qq.com>
2022-11-03 22:41:03 +0800

    命名空间

---
+++ test/test_11_03.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;//c++库实现定义在一个叫std的命名空间中
+//int main()
+//{
+//	cout << "Hello World" << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+////using namespace std;//c++库实现定义在一个叫std的命名空间中
+//int main()
+//{
+//	//指定命名空间
+//	std::cout << "Hello World" << std::endl;
+//	return 0;
+//}
+
+#include <iostream>
+//using namespace std;//c++库实现定义在一个叫std的命名空间中
+//将常用的展开
+using std::cout;
+using std::endl;
+int main()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}
