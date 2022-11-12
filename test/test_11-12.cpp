be01dad0c038fb81af93e437d5350a76b2911edc
leiyaling <3120275158@qq.com>
2022-11-12 22:44:36 +0800

    构造函数

---
+++ test/test_11-12.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Date
+{
+public:
+	Date(int year = 0, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+int main()
+{
+	Date d1;
+	return 0;
+}
