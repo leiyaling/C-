5e253bcf345711f6917caf29439d17b1ea83acaf
leiyaling <3120275158@qq.com>
2023-04-16 21:18:00 +0800

    反向迭代器

---
+++ test/STL/iterator.h
@@ -0,0 +1,47 @@
+#pragma once
+namespace lyl
+{
+	template<class Iterator, class Ref, class Ptr>
+	struct ReverseIterator
+	{
+		typedef ReverseIterator<Iterator, Ref, Ptr> Self;
+		Iterator _cur;
+
+		ReverseIterator(Iterator it)
+			:_cur(it)
+		{}
+
+		Ref operator*()
+		{
+			Iterator tmp = _cur;
+			--tmp;
+			return *tmp;
+		}
+		Self& operator++()
+		{
+			--_cur;
+			return *this;
+		}
+		Self& operator--()
+		{
+			++_cur;
+			return *this;
+		}
+		Self operator++(int)
+		{
+			Self tmp(*this);
+			--_cur;
+			return *this;
+		}
+		Self operator--(int)
+		{
+			Self tmp(*this);
+			++_cur;
+			return *this;
+		}
+		bool operator != (const Self& s)
+		{
+			return _cur != s._cur;
+		}
+	};
+}
